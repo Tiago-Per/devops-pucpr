3f0741e6943144720fdc3cd84d35b9525332afe4
Tiago Pereira <tiagojunior769@gmail.com>
2025-09-22 22:46:26 -0300

    Teste discord webhook

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ async def root():
 
 @app.get("/random_number")
 async def random_number():
-    """Generate and return a random integer between 0 and 1000."""
-    return {"return": True, "random_number": random.randint(0,1000)}
+    """Generate and return a random integer between 0 and 50000."""
+    return {"return": True, "random_number": random.randint(0,50000)}
